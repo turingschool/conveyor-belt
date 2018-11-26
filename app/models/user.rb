adbb0948e5414004c55f46512f871bdf7a7a6ac7
Josh Mejia <joshua.mejia@gmail.com>
2018-11-26 16:59:37 -0700

    Add github nickname to data stored
    Using this to remind students to add the instructor as a contributor to the repo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     User.find_or_create_by(uid: auth.uid) do |user|
+      user.nickname = auth.info.nickname
       user.token = auth.credentials.token
       user.name = auth.info.name
       user.email = auth.info.email
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181123193846) do
+ActiveRecord::Schema.define(version: 20181126221612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20181123193846) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
   end
 
   add_foreign_key "clones", "projects"
