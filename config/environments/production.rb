f72d382362c74016fdb7648c0c4fdefcfbab32ee
Josh Mejia <joshua.mejia@gmail.com>
2019-01-12 14:43:37 -0700

    Update SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'enroll.turing.io' }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    domain:               'enroll.turing.io',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
