b2764b9f10e44b72007522b4b0edd8e001cf3625
David Whitaker <damwhit@gmail.com>
2020-07-01 11:08:03 -0600

    Raise errors for mailer failure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'enroll.turing.io' }
   config.action_mailer.smtp_settings = {
