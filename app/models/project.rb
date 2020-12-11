080c92af5cd682da09deb5f5d5596df8150d42d4
ian douglas <ian.douglas@iandouglas.com>
2020-12-10 19:05:04 -0700

    wrong join association

--- app/models/project.rb
+++ app/models/project.rb
@@ -27,6 +27,6 @@ class Project < ApplicationRecord
   end
 
   def get_clones
-    clones.joins(:user).where(project: self).order('users.nickname asc, users.email asc, projects.created_at desc')
+    clones.joins(:user).where(project: self).order('users.nickname asc, users.email asc, clones.created_at desc')
   end
 end
