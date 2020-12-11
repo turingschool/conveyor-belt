9ae8a88376aedac7e6bd23114a5f25b0ea773c2a
ian douglas <ian.douglas@iandouglas.com>
2020-12-10 19:27:36 -0700

    trying to autorefresh auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,21 @@ class User < ApplicationRecord
   has_many :clones, dependent: :destroy
 
   def self.from_omniauth(auth)
-    User.find_or_create_by(uid: auth.uid) do |user|
-      is_admin = false
+    user = User.find_bu(uid: auth.uid)
+    is_admin = false
+    if user.nil?
       client = Octokit::Client.new(access_token: auth.credentials.token)
       approved_orgs = ApprovedOrganizations.all
       found_org = client.orgs.find { |org| org.login.in?(approved_orgs) }
 
       is_admin = true if found_org
-
       user.admin = is_admin
-      user.nickname = auth.info.nickname
-      user.token = auth.credentials.token
-      user.name = auth.info.name
-      user.email = auth.info.email
     end
+    user.nickname = auth.info.nickname
+    user.token = auth.credentials.token
+    user.name = auth.info.name
+    user.email = auth.info.email
+    user.save
+    user
   end
 end
