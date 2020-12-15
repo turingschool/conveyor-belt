e44afd9909decc5fd7b3737caa4ec0f57b4d5049
Brian Zanti <brianzanti@gmail.com>
2020-12-15 16:53:48 -0700

    refactors test to share setup

--- spec/features/student/student_can_create_a_clone_spec.rb
+++ spec/features/student/student_can_create_a_clone_spec.rb
@@ -1,48 +1,45 @@
 require 'rails_helper'
 
 feature 'User creates a new clone' do
-  scenario 'with valid options' do
-    instructor = create(:instructor, nickname: 'iandouglas')
-    student = create(:student)
-    project = create(:project, hash_id: 'abc123', user: instructor, project_board_base_url: 'https://github.com/turingschool/newb-tube/projects/1')
+  describe 'with valid options' do
+    before :each do
+      @student = create(:student)
+      @project = create(:project)
+      mock_login(@student)
+    end
 
-    mock_login(student)
+    scenario 'it creates successfully' do
+      expect(ProjectBoardClonerWorker).to receive(:perform_later)#.with array_including(project, clone, "bambi@example.com")
 
-    expect(ProjectBoardClonerWorker).to receive(:perform_later)#.with array_including(project, clone, "bambi@example.com")
+      visit new_project_clone_path(project_id: @project.hash_id)
 
-    visit new_project_clone_path(project_id: project.hash_id)
+      fill_in :students, with: 'Flower, Thumper, Bambi'
+      fill_in :email, with: 'bambi@example.com'
 
-    fill_in :students, with: 'Flower, Thumper, Bambi'
-    fill_in :email, with: 'bambi@example.com'
+      expect {
+        click_on 'Submit'
+      }.to change { Clone.count }.by(1)
 
-    expect {
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('Thanks for your submission!')
+    end
+
+
+
+    scenario 'student cannot create a clone when that project has been deleted by an admin' do
+      visit new_project_clone_path(project_id: @project.hash_id)
+
+      fill_in :students, with: 'Flower, Thumper, Bambi'
+      fill_in :email, with: 'bambi@example.com'
+
+      # let's imagine here that an admin deletes a project, the clone will no longer save properly
+      @project.destroy
+
       click_on 'Submit'
-    }.to change { Clone.count }.by(1)
 
-    expect(current_path).to eq(root_path)
-    expect(page).to have_content('Thanks for your submission!')
-  end
-
-  scenario 'student cannot create a clone when that project has been deleted by an admin' do
-    instructor = create(:instructor, nickname: 'iandouglas')
-    student = create(:student)
-    project = create(:project, hash_id: 'abc123', user: instructor, project_board_base_url: 'https://github.com/turingschool/newb-tube/projects/1')
-
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(student)
-    allow(ApprovedOrganizations).to receive(:all).and_return(%w(not-a-real-organization))
-
-    visit new_project_clone_path(project_id: project.hash_id)
-
-    fill_in :students, with: 'Flower, Thumper, Bambi'
-    fill_in :email, with: 'bambi@example.com'
-
-    # let's imagine here that an admin deletes a project, the clone will no longer save properly
-    project.destroy
-
-    click_on 'Submit'
-
-    expect(current_path).to eq(root_path)
-    expect(page).to have_content('we were unable to clone the project board')
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('we were unable to clone the project board')
+    end
   end
 
   describe 'when that clone already exists' do
